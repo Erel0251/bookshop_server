4972fb94a403cb435abe03bc9bfd41809cd3fd0b
Tran Duc Anh <tranducanhghu@gmail.com>
2024-06-01 14:36:46 +0700

    test: add unit test for books

--- src/modules/book/book.controller.spec.ts
+++ src/modules/book/book.controller.spec.ts
@@ -1,9 +1,43 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BookController } from './book.controller';
 import { BookService } from './book.service';
+import { CreateBookDto } from './dto/create-book.dto';
+import { BookStatus } from './constants/status.enum';
+import { HttpStatus } from '@nestjs/common';
+import { Book } from './entities/book.entity';
+
+const mockBook: CreateBookDto | Book = {
+  id: 'c7d0c154-bd0f-4d78-b4cf-697eee7b61a2',
+  title: 'test',
+  author: 'test',
+  publisher: 'test',
+  img_urls: ['test'],
+  price: 100,
+  status: BookStatus.AVAILABLE,
+  currency: 'VND',
+};
+
+const mockBookService = {
+  create: jest.fn().mockResolvedValue(mockBook),
+  findAll: jest.fn().mockResolvedValue([mockBook]),
+  findOne: jest.fn().mockResolvedValue(mockBook),
+  update: jest.fn().mockResolvedValue(mockBook),
+  remove: jest.fn().mockResolvedValue(true),
+};
+
+const mockResponse = () => {
+  const res = {
+    status: jest.fn().mockReturnThis(),
+    json: jest.fn().mockReturnThis(),
+    render: jest.fn().mockReturnThis(),
+    send: jest.fn().mockReturnThis(),
+  };
+  return res;
+};
 
 describe('BookController', () => {
   let controller: BookController;
+  let service: BookService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -11,15 +45,78 @@ describe('BookController', () => {
       providers: [
         {
           provide: BookService,
-          useValue: {},
+          useValue: mockBookService,
         },
       ],
     }).compile();
 
     controller = module.get<BookController>(BookController);
+    service = module.get<BookService>(BookService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  // Create a book
+  describe('create', () => {
+    it('should return a book', async () => {
+      const res = mockResponse();
+      await controller.create(mockBook as CreateBookDto, res);
+
+      expect(service.create).toHaveBeenCalled();
+      expect(res.status).toHaveBeenCalledWith(HttpStatus.CREATED);
+      expect(res.send).toHaveBeenCalledWith({
+        message: 'Create book successfully',
+      });
+    });
+  });
+
+  // Find all books
+  describe('findAll', () => {
+    it('should return an array of books', async () => {
+      const result = [mockBook as Book];
+      const res = mockResponse();
+      await controller.findAll(undefined, res);
+
+      expect(service.findAll).toHaveBeenCalled();
+      expect(res.status).toHaveBeenCalledWith(HttpStatus.OK);
+      expect(res.render).toHaveBeenCalledWith('book', {
+        title: 'Book',
+        message: 'Get data successfully',
+        books: result,
+        statuses: Object.values(BookStatus),
+      });
+    });
+  });
+
+  // Find a book by id
+  describe('findOne', () => {
+    it('should return a book', async () => {
+      const res = mockResponse();
+      await controller.findOne('c7d0c154-bd0f-4d78-b4cf-697eee7b61a2', res);
+
+      expect(service.findOne).toHaveBeenCalled();
+      expect(res.status).toHaveBeenCalledWith(HttpStatus.OK);
+      expect(res.send).toHaveBeenCalledWith(mockBook);
+    });
+  });
+
+  // Update a book
+  describe('update', () => {
+    it('should return a book', async () => {
+      const res = mockResponse();
+      await controller.update(
+        'c7d0c154-bd0f-4d78-b4cf-697eee7b61a2',
+        mockBook as CreateBookDto,
+        res,
+      );
+
+      expect(service.update).toHaveBeenCalled();
+      expect(res.status).toHaveBeenCalledWith(HttpStatus.OK);
+      expect(res.send).toHaveBeenCalledWith({
+        message: 'Update book successfully',
+      });
+    });
   });
 });
--- src/modules/book/book.service.spec.ts
+++ src/modules/book/book.service.spec.ts
@@ -1,23 +1,114 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { BookService } from './book.service';
+import { CreateBookDto } from './dto/create-book.dto';
+import { Book } from './entities/book.entity';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { BookStatus } from './constants/status.enum';
+import { ReviewService } from '../review/review.service';
+import { PromotionService } from '../promotion/promotion.service';
+import { CategoryService } from '../category/category.service';
+
+const mockBook: CreateBookDto = {
+  id: 'c7d0c154-bd0f-4d78-b4cf-697eee7b61a2',
+  title: 'test',
+  author: 'test',
+  publisher: 'test',
+  img_urls: ['test'],
+  price: 100,
+  status: BookStatus.AVAILABLE,
+  currency: 'VND',
+};
+
+const mockBookRepository = {
+  create: jest.fn().mockResolvedValue(mockBook),
+  save: jest.fn().mockResolvedValue(mockBook),
+  findOne: jest.fn().mockResolvedValue(mockBook),
+  findAll: jest.fn().mockResolvedValue([mockBook]),
+  update: jest.fn().mockResolvedValue(true),
+  delete: jest.fn().mockResolvedValue(true),
+  createQueryBuilder: jest.fn(() => ({
+    where: jest.fn(),
+    andWhere: jest.fn(),
+    orderBy: jest.fn(),
+    offset: jest.fn(),
+    limit: jest.fn(),
+    getMany: jest.fn().mockResolvedValue([mockBook]),
+  })),
+};
+
+const mockCategoryService = {};
+
+const mockReviewService = {};
+
+const mockPromotionService = {};
 
 describe('BookService', () => {
   let service: BookService;
+  //let repository: Repository<Book>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
+        BookService,
         {
-          provide: BookService,
-          useValue: {},
+          provide: getRepositoryToken(Book),
+          useValue: mockBookRepository,
+        },
+        {
+          provide: CategoryService,
+          useValue: mockCategoryService,
+        },
+        {
+          provide: ReviewService,
+          useValue: mockReviewService,
+        },
+        {
+          provide: PromotionService,
+          useValue: mockPromotionService,
         },
       ],
     }).compile();
 
     service = module.get<BookService>(BookService);
+    //repository = module.get<Repository<Book>>(getRepositoryToken(Book));
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  // Create a book
+  it('should create a book', async () => {
+    const book = await service.create(mockBook);
+    expect(book).toEqual(mockBook);
+  });
+
+  // Find all books
+  it('should return all books', async () => {
+    expect(await service.findAll()).toEqual([mockBook]);
+  });
+
+  // Find one book
+  it('should return one book', async () => {
+    expect(
+      await service.findOne('c7d0c154-bd0f-4d78-b4cf-697eee7b61a2'),
+    ).toEqual(mockBook);
+  });
+
+  // Update a book
+  it('should update a book', async () => {
+    const updatedBook = await service.update(
+      'c7d0c154-bd0f-4d78-b4cf-697eee7b61a2',
+      mockBook,
+    );
+    expect(updatedBook).toBeUndefined();
+  });
+
+  // Delete a book
+  it('should delete a book', async () => {
+    const deletedBook = await service.delete(
+      'c7d0c154-bd0f-4d78-b4cf-697eee7b61a2',
+    );
+    expect(deletedBook).toBeUndefined();
   });
 });
