76ad5715103c192119b971dd3370cc52715be516
Tran Duc Anh <tranducanhghu@gmail.com>
2024-06-01 15:24:04 +0700

    test: add user and auth unit test

--- src/modules/auth/auth.controller.spec.ts
+++ src/modules/auth/auth.controller.spec.ts
@@ -1,6 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { mockResponse } from '../../shared/dto/mock-response.dto';
+import { Role } from '../user/constants/role.enum';
+import { HttpStatus } from '@nestjs/common';
+import { CreateUserDto } from '../user/dto/create-user.dto';
+
+const mockUser = {
+  email: 'helloworld@gmail.com',
+  password: 'helloworld',
+  roles: [Role.USER],
+};
+
+const mockAuthService = {
+  signUp: jest.fn().mockResolvedValue(mockUser),
+  login: jest.fn().mockResolvedValue(mockUser),
+  logout: jest.fn().mockResolvedValue(true),
+};
 
 describe('AuthController', () => {
   let controller: AuthController;
@@ -11,7 +27,7 @@ describe('AuthController', () => {
       providers: [
         {
           provide: AuthService,
-          useValue: {},
+          useValue: mockAuthService,
         },
       ],
     }).compile();
@@ -21,5 +37,42 @@ describe('AuthController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  // Sign up
+  describe('signUp', () => {
+    it('should return a user', async () => {
+      const res = mockResponse();
+      await controller.signUp(mockUser as CreateUserDto, res);
+
+      expect(mockAuthService.signUp).toHaveBeenCalled();
+      expect(res.status).toHaveBeenCalledWith(HttpStatus.CREATED);
+      expect(res.cookie).toHaveBeenCalled();
+    });
+  });
+
+  // Login
+  describe('login', () => {
+    it('should return a user', async () => {
+      const res = mockResponse();
+      await controller.login(mockUser as CreateUserDto, res);
+
+      expect(mockAuthService.login).toHaveBeenCalled();
+      expect(res.status).toHaveBeenCalledWith(HttpStatus.OK);
+      expect(res.cookie).toHaveBeenCalled();
+    });
+  });
+
+  // Logout
+  describe('logout', () => {
+    it('should return true', async () => {
+      const res = mockResponse();
+      await controller.logout(mockUser.email, res);
+
+      //expect(mockAuthService.logout).toHaveBeenCalled();
+      expect(res.status).toHaveBeenCalledWith(HttpStatus.OK);
+      expect(res.clearCookie).toHaveBeenCalled();
+      expect(res.redirect).toHaveBeenCalledWith('/');
+    });
   });
 });
