a84b308eb20ad327125b988d620512f3dbf16233
Tran Duc Anh <tranducanhghu@gmail.com>
2024-06-03 21:42:31 +0700

    feat: add about apge

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Req, Res } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpStatus,
+  Post,
+  Req,
+  Res,
+} from '@nestjs/common';
+import * as fs from 'fs';
 
 @Controller()
 export class AppController {
@@ -21,5 +30,36 @@ export class AppController {
   @Get('signup')
   signup(@Res() res: any) {
     res.render('signup', { message: 'Please signup', title: 'Signup' });
+  }
+
+  @Get('about')
+  about(@Res() res: any) {
+    let data = undefined;
+    try {
+      data = fs.readFileSync('public/about.txt', 'utf8');
+    } catch (err) {
+      console.error(err);
+    }
+    res.render('about', { message: 'About page', title: 'About', data: data });
+  }
+
+  // for user get
+  @Get('about/view')
+  aboutView(@Res() res: any) {
+    let data = 'Blank data';
+    try {
+      data = fs.readFileSync('public/about.txt', 'utf8');
+    } catch (err) {
+      console.error(err);
+    }
+    res.status(HttpStatus.OK).send(data);
+  }
+
+  @Post('about')
+  postAbout(@Body() req: any, @Res() res: any) {
+    console.log(req);
+    console.log(req.editordata);
+    fs.writeFileSync('public/about.txt', req.editordata);
+    res.status(HttpStatus.OK).redirect('about');
   }
 }
