0a136181b6984273122ae77638c06ffc603c9773
Tran Duc Anh <tranducanhghu@gmail.com>
2024-06-08 14:37:07 +0700

    feat: add cloudinary

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -28,4 +28,10 @@ export default () => ({
     refreshSecret: process.env.JWT_REFRESH_SECRET,
     refreshExpires: process.env.JWT_REFRESH_EXPIRES,
   },
+  cloudinary: {
+    cloudName: process.env.CLOUDINARY_CLOUD_NAME,
+    apiKey: process.env.CLOUDINARY_API_KEY,
+    apiSecret: process.env.CLOUDINARY_API_SECRET,
+    url: process.env.CLOUDINARY_URL,
+  },
 });
